7d1adeda9c181f47342e44dadf4e85f6c06ac5be
Eddie1337 <e.verheul91@gmail.com>
2017-09-14 18:30:49 +0200

    mysqli spprt

--- includes/mysql.php
+++ includes/mysql.php
@@ -36,14 +36,14 @@ if (!defined('LICENSE'))
 
 
 
-$connection = mysql_pconnect(DBHOST, DBUSER, DBPASSWORD);	// Connection to database
+$connection = mysqli_connect(DBHOST, DBUSER, DBPASSWORD);	// Connection to database
 if (!$connection)	// Return error if connection is broken
 {
 	exit("<html><head></head><body><b>Critical Error!!!</b><br />MySQL Error!</body></html>");
 }
 
 
-$db_connection = mysql_select_db(DBNAME);	// Select our database
+$db_connection = mysqli_select_db(DBNAME);	// Select our database
 if (!$db_connection)	// Return error	if error happened with database
 {
 	exit("<html><head></head><body><b>Critical Error!!!</b><br />MySQL Error!</body></html>");
@@ -58,14 +58,17 @@ if (!$db_connection)	// Return error	if error happened with database
  *
  * No return.
  */
-function query_basic($query)
-{
-	$result = mysql_query($query);
+function query_basic($query) {
+	$conn = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBNAME);
+	$result = mysqli_query($conn, $query);
 	if ($result == FALSE)
 	{
-		$msg = 'Invalid query : '.mysql_error()."\n";
+		$msg = 'Invalid query : '.mysqli_error($conn)."\n";
 		echo $msg;
+		return FALSE;
 	}
+	else
+		return TRUE;
 }
 
 /**
@@ -73,15 +76,15 @@ function query_basic($query)
  *
  * Retrieves the number of rows from a result set and return it.
  */
-function query_numrows($query)
-{
-	$result = mysql_query($query);
+function query_numrows($query) {
+	$conn = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBNAME);
+	$result = mysqli_query($conn, $query);
 	if ($result == FALSE)
 	{
-		$msg = 'Invalid query : '.mysql_error()."\n";
+		$msg = 'Invalid query : '.mysqli_error($conn)."\n";
 		echo $msg;
 	}
-	return (mysql_num_rows($result));
+	return (mysqli_num_rows($result));
 }
 
 /**
@@ -89,14 +92,14 @@ function query_numrows($query)
  *
  * Returns an associative array that corresponds to the fetched row.
  */
-function query_fetch_assoc($query)
-{
-	$result = mysql_query($query);
+function query_fetch_assoc($query) {
+	$conn = mysqli_connect(DBHOST, DBUSER, DBPASSWORD, DBNAME);
+	$result = mysqli_query($conn, $query);
 	if ($result == FALSE)
 	{
-		$msg = 'Invalid query : '.mysql_error()."\n";
+		$msg = 'Invalid query : '.mysqli_error($conn)."\n";
 		echo $msg;
 	}
-	return (mysql_fetch_assoc($result));
+	return mysqli_fetch_assoc($result);
 }
 ?>
